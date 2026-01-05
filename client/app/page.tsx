5d611c94394432dd52b3e22a7de949edf89073d8
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2026-01-05 15:34:34 +0700

    style(client): add register page styling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,42 +1,12 @@
-"use client";
+import Link from "next/link";
 
-import { signup } from "@/app/actions/auth";
-import { useActionState } from "react";
-
-export default function SignupForm() {
-  const [state, action, pending] = useActionState(signup, undefined);
-
+const Homepage = () => {
   return (
-    <form action={action}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" placeholder="Name" />
-      </div>
-      {state?.errors?.name && <p>{state.errors.name}</p>}
-
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" placeholder="Email" />
-      </div>
-      {state?.errors?.email && <p>{state.errors.email}</p>}
-
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      {state?.errors?.password && (
-        <div>
-          <p>Password must:</p>
-          <ul>
-            {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <button disabled={pending} type="submit">
-        Sign Up
-      </button>
-    </form>
+    <div>
+      <h1>Homepage</h1>
+      <Link href="/register">Register</Link>
+    </div>
   );
-}
+};
+
+export default Homepage;
